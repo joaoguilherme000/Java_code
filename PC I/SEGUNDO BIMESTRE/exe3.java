ebcb71989d5374c3e202db1baa212f6d4ebe3c63
joaoguilherme <joaoguilhermehig@gmail.com>
2023-10-05 17:42:45 -0300

    exe3 feito

--- PC I/SEGUNDO BIMESTRE/exe3.java
+++ PC I/SEGUNDO BIMESTRE/exe3.java
@@ -38,3 +38,31 @@ public class exe1 {
 }
 
 /*BASICAMENTE VOCE TEM QUE ARRUMAR O CODIGO, EM CIMA O ANTIGO E EM BAIXO O NOVO*/
+
+import java.util.Scanner;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+		double [] valor = new  double [7];
+		
+		for(int i = 0 ; i< 7 ;i++){
+			
+			System.out.println("\nDigite o  numero -> " + (i+1));
+			valor[i] = scanner.nextDouble();
+			
+			if(valor[i] % 2 ==0  && valor[i] % 3 ==0  ){
+				System.out.println(" valor divisivel por 2 e 3");
+			}else if(valor[i] % 2==0  ){
+				System.out.println(" valor divisivel por 2");
+			}else if(valor[i] % 3==0  ){
+				System.out.println(" valor divisivel por 3");
+			}else{
+				System.out.println(" valor não divisivel nem por 2 e nem por 3");
+            }
+		}
+	}
+}
