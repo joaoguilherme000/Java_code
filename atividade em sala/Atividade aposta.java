781a656e86a5d06fb5a63f52b42538aa650ccad0
João Guilherme <132224010+joaoguilherme000@users.noreply.github.com>
2023-10-17 21:15:48 -0300

    arrumado

--- atividade em sala/Atividade aposta.java
+++ atividade em sala/Atividade aposta.java
@@ -1,3 +1,5 @@
+
+
 package main;
 
 import javax.swing.JOptionPane;
@@ -23,85 +25,11 @@ public class Main {
                     Menu.lotofacil();
                     break;
                 case 5:
-                    JOptionPane.showMessageDialog(null, "Saindo...6"
-                            + ""
-                            + "");
+                    JOptionPane.showMessageDialog(null, "Saindo...6");
                     return; // Sai do método e encerra o programa
                 default:
-                    System.out.println("Opção inválida. Tente novamente.");
+                    JOptionPane.showMessageDialog(null, "Escolha uma opção valida !");
             }
         }
     }
-}
-
-
-// A OUTRA CLASSE
-
-
-package main;
-
-import java.util.Random;
-import javax.swing.JOptionPane;
-
-
-public class Menu {
-    
-    public static void quina(){
-        Random random = new Random();
-        String mensagem = "";
-        int[] vetor = new int[5];
-
-        for(int i =0; i < 5; i++){
-          vetor[i] = random.nextInt(80) + 1; 
-          mensagem += vetor[i] + " | ";
-        }
-
-        JOptionPane.showMessageDialog(null, "Seu jogo: \n" + mensagem);
-
-    }
-    
-    public static void megasena(){
-        String mensagem = "";
-        Random random = new Random();
-        int[] vetor = new int[5];
-
-        for(int i =0; i < 5; i++){
-          vetor[i] = random.nextInt(80) + 1; 
-          mensagem += vetor[i] + " | ";
-        }
-        
-        JOptionPane.showMessageDialog(null, "Seu jogo: \n" + mensagem);
-        
-    }
-    
-    public static void duplasena(){
-        String mensagem = "";
-        Random random = new Random();
-        int[] vetor = new int[6];
-
-        for(int i =0; i < 6; i++){
-          vetor[i] = random.nextInt(50) + 1; 
-          mensagem += vetor[i] + " | ";
-        }
-        
-        JOptionPane.showMessageDialog(null, "Seu jogo: \n" + mensagem);
-    }
-    
-    public static void lotofacil(){
-        String mensagem = "";
-        Random random = new Random();
-        int[] vetor = new int[15];
-        int[] vetor2 = new int[15];
-        String mensagem2 = "";
-
-        for(int i =0; i < 15; i++){
-          vetor[i] = random.nextInt(25) + 1; 
-          mensagem += vetor[i] + " | ";
-          vetor2[i] = random.nextInt(25) + 1; 
-          mensagem2 += vetor2[i] + " | ";
-        }
-        
-        JOptionPane.showMessageDialog(null, "Seu jogo: \n\n" + mensagem +"\n" + mensagem2);
-    }
-    
 }
