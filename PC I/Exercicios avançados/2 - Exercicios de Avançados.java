71f2532b005eb7e29c5a9a4bb85b1e970551a99f
João Guilherme <132224010+joaoguilherme000@users.noreply.github.com>
2023-10-17 22:18:59 -0300

    Update 2 - Exercicios de Avançados.java

--- PC I/Exercicios avançados/2 - Exercicios de Avançados.java
+++ PC I/Exercicios avançados/2 - Exercicios de Avançados.java
@@ -36,7 +36,6 @@ public class Main {
 
 // A OUTRA CLASSE
 
-
 package main;
 
 import java.util.Random;
@@ -91,23 +90,43 @@ public class Menu {
             switch(escolhaInt){
                 case 1:
                     while(true){
-                        cont ++;
-                        System.out.println(meuNome + cont);
-                        if(cont == 100){
-                            break;
-                        }
+                        double base = 2.0;
+                        double expoente = 3.0;
+
+                        double resultado = Math.pow(base, expoente);
+
+                        JOptionPane.showMessageDialog(null, base + " elevado a " + expoente + " é igual a " + resultado);
+                        break;
                     }
                     break;
                 case 2:
                     do{
-                        cont ++;
-                        System.out.println(meuNome + cont);
-                    }while(cont < 100);
+                        String entradaNumero = JOptionPane.showInputDialog(null, "Numero :");
+                        int numero = Integer.parseInt(entradaNumero);
+                        
+                        String entradaExpoente = JOptionPane.showInputDialog(null, "Expoente :");
+                        int expoente = Integer.parseInt(entradaExpoente);;
+
+                        double resultado = Math.pow(numero, expoente);
+
+                        JOptionPane.showMessageDialog(null, numero + " elevado a " + expoente + " é igual a " + resultado);
+                        cont++;
+                    }while(cont < 1);
                     break;
                 case 3:
-                    for(int i =0; i < 100; i++){
-                        System.out.println(meuNome + i);
+                    String entradaNumero = JOptionPane.showInputDialog(null, "Numero :");
+                    int numero = Integer.parseInt(entradaNumero);
+
+                    String entradaExpoente = JOptionPane.showInputDialog(null, "Expoente :");
+                    int expoente = Integer.parseInt(entradaExpoente);
+
+                    int resultado = 1;
+
+                    for (int i = 0; i < expoente; i++) {
+                        resultado *= numero;
                     }
+
+                    JOptionPane.showMessageDialog(null, numero + " elevado a " + expoente + " é igual a " + resultado);
                     break;
                 case 5:
                     JOptionPane.showMessageDialog(null, "Saindo...");
