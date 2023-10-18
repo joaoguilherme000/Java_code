537762f3f47b7b9838462cd6ba60a9f685c61b9c
João Guilherme <132224010+joaoguilherme000@users.noreply.github.com>
2023-10-17 21:40:27 -0300

    Update Atividade aposta.java

--- atividade em sala/Atividade aposta.java
+++ atividade em sala/Atividade aposta.java
@@ -33,3 +33,74 @@ public class Main {
         }
     }
 }
+
+// A outra classe 
+
+
+package main;
+
+import java.util.Random;
+import javax.swing.JOptionPane;
+
+
+public class Menu {
+    
+    public static void quina(){
+        Random random = new Random();
+        String mensagem = "";
+        int[] vetor = new int[5];
+
+        for(int i =0; i < 5; i++){
+          vetor[i] = random.nextInt(80) + 1; 
+          mensagem += vetor[i] + " | ";
+        }
+
+        JOptionPane.showMessageDialog(null, "Seu jogo: \n" + mensagem);
+
+    }
+    
+    public static void megasena(){
+        String mensagem = "";
+        Random random = new Random();
+        int[] vetor = new int[5];
+
+        for(int i =0; i < 5; i++){
+          vetor[i] = random.nextInt(80) + 1; 
+          mensagem += vetor[i] + " | ";
+        }
+        
+        JOptionPane.showMessageDialog(null, "Seu jogo: \n" + mensagem);
+        
+    }
+    
+    public static void duplasena(){
+        String mensagem = "";
+        Random random = new Random();
+        int[] vetor = new int[6];
+
+        for(int i =0; i < 6; i++){
+          vetor[i] = random.nextInt(50) + 1; 
+          mensagem += vetor[i] + " | ";
+        }
+        
+        JOptionPane.showMessageDialog(null, "Seu jogo: \n" + mensagem);
+    }
+    
+    public static void lotofacil(){
+        String mensagem = "";
+        Random random = new Random();
+        int[] vetor = new int[15];
+        int[] vetor2 = new int[15];
+        String mensagem2 = "";
+
+        for(int i =0; i < 15; i++){
+          vetor[i] = random.nextInt(25) + 1; 
+          mensagem += vetor[i] + " | ";
+          vetor2[i] = random.nextInt(25) + 1; 
+          mensagem2 += vetor2[i] + " | ";
+        }
+        
+        JOptionPane.showMessageDialog(null, "Seu jogo: \n\n" + mensagem +"\n" + mensagem2);
+    }
+    
+}
